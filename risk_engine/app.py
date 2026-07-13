a2146e0bb855a75257c94b807d58036ac6a849f3
Khushii <155296248+sushi286@users.noreply.github.com>
2026-07-13 16:11:18 +0530

    updated

--- risk_engine/app.py
+++ risk_engine/app.py
@@ -5,16 +5,14 @@ from routers import risk, alerts, stats, history
 
 app = FastAPI(title="SentinelX - Risk Engine (Team 3)")
 
-# Allow frontend (running on a different port) to talk to this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for development; restrict this later in production
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Connect all routers
 app.include_router(risk.router)
 app.include_router(alerts.router)
 app.include_router(stats.router)
