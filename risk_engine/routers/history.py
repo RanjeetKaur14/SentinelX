bbafddef158cfb1f1469c74f8f6c0b7d0ad87683
Ranjeet <ranjeet.kaur@outlook.in>
2026-07-13 15:57:40 +0530

    Keep people detection module only

--- risk_engine/routers/history.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from database import get_all_history
-
-router = APIRouter()
-
-
-@router.get("/history")
-def get_history():
-    return {"history": get_all_history()}
